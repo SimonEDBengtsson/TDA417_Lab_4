d7565fd37d82e4cda90bbaaec4d660570b2f3ca5
Simon Bengtsson <ssa@tulebo.se>
2019-12-11 13:59:06 +0100

    Cleanup and comments

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -87,31 +87,40 @@ public class PathFinder<V> {
         return new Result<>(false, start, null, -1, null, visitedNodes);
     }
 
+    /**
+     * A queue that keeps track of the cost of inserted elements and builds a minimum spanning tree
+     */
     private class SearchQueue {
         MappingComparator comparator=new MappingComparator();
         PriorityQueue<V> priorityQueue=new PriorityQueue<>(comparator);
-        Map<V,V> precedingMap =new HashMap<>();
+        Map<V,V> precedingMap =new HashMap<>();// basis for the MST
 
+        /**
+         * A comparator with an internal map of ordinal numbers for the objects to be compared
+         */
         private class MappingComparator implements Comparator<V> {
             Map<V,Double> costMap=new HashMap<>();
 
-            boolean contains(V key) {
-                return costMap.containsKey(key);
-            }
-
+            /**
+             * Accesses the cost mapped to a specified key
+             * @param key The key who's cost should be accessed
+             * @return A Double with the mapped cost, or null if no such mapping exists
+             */
             Double get(V key) { return costMap.get(key); }
 
-            boolean put(V key, double cost) {
+            /**
+             * Maps the specified key to the specified cost, but only if the specified cost is lower than the one already present, or the specified key does not have an associated cost
+             * @param key The key
+             * @param cost The new cost
+             * @return true iff the cost was updated
+             */
+            boolean offer(V key, double cost) {
                 Double currentCost=costMap.get(key);
                 if (currentCost==null || cost<currentCost) {
                     costMap.put(key,cost);
                     return true;
                 }
                 return false;
-            }
-
-            void remove (V key) {
-                costMap.remove(key);
             }
 
             @Override
@@ -121,26 +130,42 @@ public class PathFinder<V> {
             }
         }
 
+        /**
+         * Attempts to insert an element into the queue with a specified cost, with a specified point of attachment
+         * @param element The element to be inserted
+         * @param cost The cost to insert the element with
+         * @param tail The element to consider preceding in the spanning tree
+         */
         void offer(V element, double cost, V tail) {
-            if (comparator.contains(element)) {
-                priorityQueue.remove(element);
-            }
-            if (comparator.put(element,cost)) {
-                precedingMap.put(element,tail);
-            }
+            if (!comparator.offer(element,cost)) return;
+            assert precedingMap.containsKey(tail) || tail==null;
+            precedingMap.put(element,tail);
+            priorityQueue.remove(element);
             priorityQueue.offer(element);
         }
 
-        V poll() {
-            return priorityQueue.poll();
-        }
+        /**
+         * Remove and return the smallest element currently in the queue
+         * @return The smallest element in the queue
+         */
+        V poll() { return priorityQueue.poll(); }
 
+        /**
+         * The cost the queue associates with the specified element
+         * @param element The element who's cost to check
+         * @return The stored cost, or infinity if there is none
+         */
         double cost(V element) {
             Double cost=comparator.get(element);
-            if (cost==null) return Double.MAX_VALUE;
+            if (cost==null) return Double.POSITIVE_INFINITY;
             else return cost;
         }
 
+        /**
+         * Generates an ordered list of the path stored in the MST from a root node to the specified element
+         * @param element The element to which a path is to be generated
+         * @return The generated path to the specified element
+         */
         List<V> path(V element) {
             List<V> reversePath=new ArrayList<>();
             V preceding=element;
@@ -164,8 +189,6 @@ public class PathFinder<V> {
             return list;
         }
     }
-
-    // public Result(boolean success, V start, V goal, double cost, List<V> path, int visitedNodes) {
 
     public Result<V> searchDijkstra(V start, V goal) {
         int visitedNodes = 0;
